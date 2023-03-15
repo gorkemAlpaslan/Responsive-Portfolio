6f3b730f09870999947949698f805e290955dda1
gorkemAlpaslan <gorkem.alpaslan98@gmail.com>
2023-03-15 16:07:40 +0300

    fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,6 @@ import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import LinkIcon from "@mui/icons-material/Link";
 import emailjs from "@emailjs/browser";
 import React from "react";
-import Projects from "./Projects";
 
 export default function Home() {
   const [pageIsLoad, SetpageIsLoad] = useState<undefined | string>("menu");
@@ -34,6 +33,43 @@ export default function Home() {
       SetpageIsLoad(undefined);
     }
   };
+
+  const Projects = [
+    {
+      id: 1,
+      Name: "SplendShop",
+      Link: "https://graceful-flan-8ed3f8.netlify.app/homepage",
+      Details:
+        "SplendShop is a basic e-commercial site that is designed and coded by me. I can tell that journey of becoming a React developer started by coding this web app, so the code may not be that clean but SplendShop is working and it improved me realy good to me.",
+      CodeDetails:
+        "I used ContextAPI, Firebase for authentication, MUI for some components and styles, some libraries for carousel and pagination, I used Local Storage to manage purchase and favorite Items",
+    },
+    {
+      id: 2,
+      Name: "To Do List",
+      Link: "https://enchanting-tapioca-2145c3.netlify.app",
+      Details:
+        "This project was first case of a practicum (Patika - PopUpSmart) that user can Add/Delete/Edit to do list items, mark them as complated and list according to different situations ",
+      CodeDetails:
+        "I used a mockAPI to keep to do list data only, I know that the authentication system was not reliable and accurate because it kept the user data locally, but it was asked to do so in the study.",
+    },
+    {
+      id: 3,
+      Name: "Expense List",
+      Link: "https://jade-baklava-7a2e68.netlify.app",
+      Details:
+        "Expense list is a basic project that user can Add new expenses with a title, amount and date. Then user can check his/her expenses listed and also a Bar Graph of them monthly.",
+      CodeDetails: "",
+    },
+    {
+      id: 4,
+      Name: "Popup Generator",
+      Link: "https://storied-cobbler-6dd078.netlify.app",
+      Details:
+        "This project was final case of a practicum (Patika - PopUpSmart). the logic is providing a enviroment that any user can select a template and customize a popup screen and get the script code of the popup he/she customized",
+      CodeDetails: "",
+    },
+  ];
 
   const sendEmailHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
